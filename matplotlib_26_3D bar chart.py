2c1a79aa1b3bbee1469f42014dbd757c5dc6f93b
CipherN <gjw098@163.com>
2019-04-09 12:04:03 +0800

    First commit

--- matplotlib_26_3D bar chart.py
+++ matplotlib_26_3D bar chart.py
@@ -1,22 +1,24 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import style
-
 style.use('ggplot')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [5, 6, 7, 8, 2, 5, 6, 3, 7, 2]
-z = [1, 2, 6, 3, 2, 7, 3, 3, 7, 2]
+x3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# y3 = [5, 6, 7, 8, 2, 5, 6, 3, 7, 2]
+z3 = np.zeros(10)
 
-x2 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-y2 = [-5, -6, -7, -8, -2, -5, -6, -3, -7, -2]
-z2 = [1, 2, 6, 3, 2, 7, 3, 3, 7, 2]
+dx = np.ones(10)
+# dx[0] = 2
 
-ax1.scatter(x, y, z, c='g', marker='o')
-ax1.scatter(x2, y2, z2, c='r', marker='o')
+dy = np.ones(10)
+dz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+ax1.bar3d(x3, y3, z3, dx, dy, dz)
 
 ax1.set_xlabel('x axis')
 ax1.set_ylabel('y axis')
